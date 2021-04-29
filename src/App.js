120b282c50494d4aff264de2fdc70e1c7ba183b8
rebeccaaaaaaaaaaa <71036610+rebeccaaaaaaaaaaa@users.noreply.github.com>
2021-04-28 22:41:51 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header.js'
+import Global from  './theme/global.js';
+
+function App() {
+  return (
+    <>
+      <Global/>
+      <Header/> 
+    </>
+  );
+}
+
+export default App;
